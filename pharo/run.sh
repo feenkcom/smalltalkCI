1104f8e48072abe7f3710d6e0dad8ad9741a60a5
George Ganea <georgeganea@gmail.com>
2018-11-01 06:57:42 +0200

    removed after download image resizing

--- pharo/run.sh
+++ pharo/run.sh
@@ -203,7 +203,6 @@ pharo::prepare_image() {
 
   print_info "Preparing Pharo image..."
   cp "${target}/"*.image "${SMALLTALK_CI_IMAGE}"
-  sh ../../../smalltalkCI-master/pharo/resizeImage.sh "${SMALLTALK_CI_IMAGE}" 800 600
   cp "${target}/"*.changes "${SMALLTALK_CI_CHANGES}"
   if ls "${target}/"*.sources 1> /dev/null 2>&1; then
     cp "${target}/"*.sources "${SMALLTALK_CI_BUILD}"
